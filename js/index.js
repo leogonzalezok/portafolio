826752732f9c38e464a924c53290dd8eb0607981
Leonel Gonzalez <leogonzalez777.lg@gmail.com>
2021-12-02 01:27:14 -0300

    Cambios en about section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(window).on('load',function(){
   gsap.to('#loader',1,{y:"-100%"});
-  delay(1000);
   gsap.to('#loader',1,{opacity:0});
   gsap.to('#loader',0,{display:"none",delay:1});
   gsap.to('#header',0,{display:"block",delay:1})
